0c536cf8b3bc563ed1962e02bf23d3050e2ce315
Mark Learst <learst@me.com>
2019-08-11 02:42:53 -0400

    Base Pages with simple navigation

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <>
+        <h2>About</h2>
+        <p>This app uses React.</p>
+        <a href="/">Home</a>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
