80892c9945064a87c8f9abbba986427222dc19fb
Mark Learst <learst@me.com>
2019-08-11 21:58:37 -0400

    Check Form validation onSubmit

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse({
+      ...course,
+      [name]: value
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved!");
+    });
+  }
+
+  function formIsValid() {
+    const _errors = {};
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author ID is required";
+    if (!course.category) _errors.category = "Category is required";
+    setErrors(_errors);
+    // Form is valid with no obj props
+    return Object.keys(_errors).length === 0;
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      {props.match.params.slug}
+      <CourseForm
+        course={course}
+        errors={errors}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
